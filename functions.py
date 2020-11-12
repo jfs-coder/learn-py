a8fd6891d9df182a24f5347697c37f7ecfa28a4e
jfs-coder <jfs-coder@protonmail.com>
2020-11-12 04:24:04 +0000

    added functions

---
+++ functions.py
@@ -0,0 +1,15 @@
+# Example functions
+def userName():
+	name = input("Please input your name:")
+	return name
+
+print(userName())
+
+# Passing data into variables
+def tryhackme(creator, assignment):
+	print(creator, assignment)
+	return
+name = input("Input creators name:")
+assignment = input("Input room assignment:")
+
+tryhackme(name, assignment)
