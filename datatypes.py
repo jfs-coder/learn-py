bcc304fbc04109c75ddd24f8bd8d9f18774b9202
jfs-coder <jfs-coder@protonmail.com>
2020-11-12 04:18:44 +0000

    datatypes added

---
+++ datatypes.py
@@ -0,0 +1,30 @@
+isRunning = True;
+name = "Meline"
+num = 55
+floatie = 4.6
+pyList = [1,2,3,4]
+pyDict = {1:"one", 2:"two", 3:"three"}
+
+print(isRunning,name,num,floatie,pyList,pyDict)
+
+#Global Variable
+ROLE = "BUG 0xD"
+
+def discord():
+	#Local Variable
+	return "https://discord.gg/Tqx6CD"
+
+# Storing user inputs
+exName = input("What is your god damn name?:")
+print("Your namne is", exName)
+
+#changing variables
+a = int(input("input a number:"))
+print(type(a))
+
+
+hmm = "6"
+print(type(hmm))
+
+print(type(int(hmm)))
+
