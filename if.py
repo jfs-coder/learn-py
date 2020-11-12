97b952b4a5a5cb2789aaa18f796cdb99a1238273
jfs-coder <jfs-coder@protonmail.com>
2020-11-12 04:33:04 +0000

    added if

---
+++ if.py
@@ -0,0 +1,23 @@
+a = 1
+
+if a == 1:
+	print('a is equal to 1')
+else:
+	print('error')
+
+name = 'optional'
+
+if name == 'optional':
+	print('optional created this room')
+elif name == 'skidy':
+	print('all praise our lord and saviour')
+else:
+	print('blue is not broken')
+
+zname = "pears"
+zrole = "cactus"
+
+if zname == "pears" and zrole == "cactus":
+	print("the one true cactus")
+else:
+	print("imposter")
