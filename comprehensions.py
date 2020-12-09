db7279e2fefbf3e56972841f2e9036a23114fec4
jfs-coder <jfs-coder@protonmail.com>
2020-12-09 17:46:08 +0000

    added a few examples

--- comprehensions.py
+++ comprehensions.py
@@ -11,3 +11,10 @@ print(x)
 # puts 100 'a' in a list
 z = ['a' for x in range(100)]
 print(z)
+# populate lists of lists
+d = [[7 for x in range(10)] for x in range(5)]
+print(d)
+# add conditions (modulo example here)
+m = [i for i in range(100) if i % 5 == 0] # will print out 0, 5, 10, 15, ... 95
+print(m)
+
