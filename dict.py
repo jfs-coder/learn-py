43a33e300fcadd1ed374a5de93d27ce3ac3638ed
jfs-coder <jfs-coder@protonmail.com>
2020-12-09 17:19:55 +0000

    testing out dict stuff

--- dict.py
+++ dict.py
@@ -9,8 +9,17 @@ print(orders["amount"])
 
 orders["orderType"] = "SELL"
 orders["amount"] = 0.357
+#add new dict entries
+orders["time"] = "19:04"
+orders["orderID"] = 15734
 
 #print(orders)
 print(orders["orderType"])
 print(orders["amount"])
+print(orders["time"])
+print(orders["orderID"])
+
+#print the whole orders dict
+print('-----')
+print(orders)
 
