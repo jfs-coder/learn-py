d2615e019a926b64d24441e1b3670e0f9d59139f
jfs-coder <jfs-coder@protonmail.com>
2020-12-09 19:58:42 +0000

    practicing kwargs and unpacking

---
+++ kwargs.py
@@ -0,0 +1,49 @@
+# *args and **kwargs
+def sep():
+    print('----')
+
+def funk():
+    def funky():
+        print('2nd function being called')
+    return funky
+funk()()
+sep()
+x = [1,5,3,78,34]
+# unpack operator *  ... pointers?
+print(x)
+sep()
+print(*x)
+sep()
+print(x[0])
+sep()
+
+def disp(x,y):
+    print(x,y)
+
+pairs = [(1,2), (3,4)]
+# non-pythonic way of doing it, but works.
+for x in pairs:
+    disp(x[0], x[1])
+sep()
+# pythonic way of going about it.
+for x in pairs:
+    disp(*x)
+sep()    
+# works with dicts
+disp(**{'x': 5, 'y': 6}) # dicts need two * asterisks
+sep()
+disp(**{'y': 6, 'x': 5}) # order of keys doesn't matter
+
+def func(*args, **kwargs):
+    print(args, kwargs) 
+    print(*args)
+    print(*kwargs)
+    # print(**kwargs)  <- doesn't work 
+
+func(1,2,3,4,5, one=0, two=1)
+
+
+
+
+                                                                                
+
