304a7e5e66818ca30ffed6c86c160130f45a6a41
jfs-coder <jfs-coder@protonmail.com>
2020-12-09 16:19:17 +0000

    playing with slice feature

---
+++ slice.py
@@ -0,0 +1,12 @@
+#slices use similar interface as for i in range(start:stop:increment)
+
+data = [1,2,3,4,5]
+
+x = data[::-1]
+y = data[2:]
+z = data[:3]
+
+print(data)
+print(x)
+print(y)
+print(z)
