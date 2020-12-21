6434ffe644cebd989cfbddb065e0bae82d46d86c
jfs-coder <jfs-coder@protonmail.com>
2020-12-20 23:22:14 -0500

    added a get_info() function to the classes

--- classes.py
+++ classes.py
@@ -2,21 +2,25 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        
+    def get_info(self):
+        print(f'Name: {self.name}  Age: {self.age}')
 
 p1 = Person("John", 36)
 
-print(p1)
-print(p1.name)
-print(p1.age)
+p1.get_info()
 
 class Order: 
     def __init__(self, orderType, amount):
         self.orderType = orderType
         self.amount = amount
+        
+    def get_info(self):
+        print(f'Order Type: {self.orderType}  Amount: {self.amount}')
 
 orderTable = Order("SELL", 0.375)
 
-print(orderTable)
+orderTable.get_info()
 
-print(orderTable.__dict__)
+
 
